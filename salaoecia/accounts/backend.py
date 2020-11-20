5b78c84dca480cc6baf32dd331101979aec1a8a4
Nicolas Marinoni Grande <nicolas.grande@inteliger.com.br>
2020-11-19 21:27:21 -0300

    tirar console log e ajustar login

--- salaoecia/accounts/backend.py
+++ salaoecia/accounts/backend.py
@@ -7,7 +7,7 @@ class QuantidadeLoginView(BaseBackend):
     def authenticate(self, request, username=None, password=None):
         user = User.objects.all().filter(username=username).first()
         if user is not None:
-            if user.check_password(raw_password=password):
+            if not user.check_password(raw_password=password):
                 if user.tentativas_login is None:
                     user.tentativas_login = 1
                 else:
